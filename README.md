5446a5468eacce0b961ec5f8037b08a427075e6c
Deepak Bhaskaran <deepak@duo.com>
2017-08-30 15:53:02 -0400

    First  working version of scripts that take a search criteria, location and return results from Yelp

---
+++ README.md
@@ -0,0 +1,7 @@
+```mkvirtualenv data_yelp```
+
+```pip install -r requirements```
+
+```./generate_list.sh```
+
+```cat names.csv```
---
+++ generate_list.sh
@@ -0,0 +1,11 @@
+#####To generate store info for a new city, state - copy a line and just edit the location value
+python simple.py --term 'Indian Grocery' --location 'Ann Arbor, MI'
+python simple.py --term 'Indian Grocery' --location 'Detroit, MI'
+python simple.py --term 'Indian Grocery' --location 'San Fransisco, CA'
+python simple.py --term 'Indian Grocery' --location 'New York City, NY'
+python simple.py --term 'Indian Grocery' --location 'Jersey City, NJ'
+python simple.py --term 'Indian Grocery' --location 'Edison, NJ'
+python simple.py --term 'Indian Grocery' --location 'Rochester Hills, NY'
+python simple.py --term 'Indian Grocery' --location 'Los Angeles, CA'
+python simple.py --term 'Indian Grocery' --location 'Philadelphia, PA'
+python simple.py --term 'Indian Grocery' --location 'Boston, MA'
