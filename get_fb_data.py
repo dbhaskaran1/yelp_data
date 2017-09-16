7d0311bbed9b1169c18a02a2246abd758ecbbdeb
Deepak Bhaskaran <deepak@duo.com>
2017-09-16 12:07:52 -0400

    Added code to get stores from all states

--- get_fb_data.py
+++ get_fb_data.py
@@ -26,18 +26,34 @@ class FacebookData(object):
 
         return pages
 
-    def print_page_data(self, places, number_of_pages=1):
-        for place in places['data']:
+    def iterate_through_pages(self, data):
+        for place in data:
             try:
                 if place['emails'] and place['location']['country'] == 'United States':
                     print '{}, {}, {}'.format( ','.join(place['emails']), place['name'], place['location']['city'])
             except:
                 pass
-        print "----------------"
 
+    def print_page_data(self, places, number_of_pages=1):
+        self.iterate_through_pages(places['data'])
+
+        try:
+            if number_of_pages > 1:
+                number_of_pages -= 1
+                pages = requests.get(places['paging']['next']).json()
+                self.iterate_through_pages(pages['data'])
+        except:
+            pass
 
 if __name__ == '__main__':
     fb1 = FacebookData()
-    pages = fb1.search_for_pages('Indian Grocery Michigan')
-    fb1.print_page_data(pages, 5)
-
+    states_list = [ 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
+					'Georgia', 'Hawaii', 'Idaho', 'Illinois Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+					'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana Nebraska', 'Nevada',
+					'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma',
+					'Oregon', 'Pennsylvania Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+					'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming' ]
+    #states_list = ['Texas', 'California']
+    for state in states_list:
+        pages = fb1.search_for_pages('Indian Groceries' + state)
+        fb1.print_page_data(pages, 20)
