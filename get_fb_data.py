ce326eab46dce5352da01a377aad8085c78a6f89
Deepak Bhaskaran <deepak@duo.com>
2017-09-16 10:33:54 -0400

    Added OOP version of the get_fb_data.py

--- get_fb_data.py
+++ get_fb_data.py
@@ -3,67 +3,41 @@ import requests
 import ConfigParser
 
 
-config = ConfigParser.ConfigParser()
-config.read('config/client.cfg')
+class FacebookData(object):
+    def __init__(self):
+        config = ConfigParser.ConfigParser()
+        config.read('config/client.cfg')
 
-access_token=config.get('fb','access_token')
-fb_api_version=config.get('fb','fb_api_version')
+        access_token=config.get('fb','access_token')
+        fb_api_version=config.get('fb','fb_api_version')
 
-graph = facebook.GraphAPI(
+        self.graph = facebook.GraphAPI(
         access_token=access_token,
         version=fb_api_version)
 
-places = graph.search(
-        type='page',
-        q='Indian Grocery Florida',
-        center='0,0',
-        distance=5000,
-        fields='name, emails, location, about')
+    def search_for_pages(self, string_to_search_for):
+        graph = self.graph
+        pages = graph.search(
+            type='page',
+            q=string_to_search_for,
+            center='0,0',
+            distance=5000,
+            fields='name, emails, location, about')
 
-#print places['paging']['next']
+        return pages
 
-for place in places['data']:
-    try:
-        if place['emails'] and place['location']['country'] == 'United States':
-            print '{}, {}, {}'.format( ','.join(place['emails']), place['name'], place['location']['city'])
-    except:
-        pass
+    def print_page_data(self, places, number_of_pages=1):
+        for place in places['data']:
+            try:
+                if place['emails'] and place['location']['country'] == 'United States':
+                    print '{}, {}, {}'.format( ','.join(place['emails']), place['name'], place['location']['city'])
+            except:
+                pass
+        print "----------------"
 
-places1 = requests.get(places['paging']['next']).json()
-for place in places1['data']:
-    try:
-        if place['emails'] and place['location']['country'] == 'United States':
-            print '{}, {}, {}'.format( ','.join(place['emails']), place['name'], place['location']['city'])
-    except:
-        pass
-places2 = requests.get(places1['paging']['next']).json()
-for place in places2['data']:
-    try:
-        if place['emails'] and place['location']['country'] == 'United States':
-            print '{}, {}, {}'.format( ','.join(place['emails']), place['name'], place['location']['city'])
-    except:
-        pass
 
-places3 = requests.get(places2['paging']['next']).json()
-for place in places3['data']:
-    try:
-        if place['emails'] and place['location']['country'] == 'United States':
-            print '{}, {}, {}'.format( ','.join(place['emails']), place['name'], place['location']['city'])
-    except:
-        pass
+if __name__ == '__main__':
+    fb1 = FacebookData()
+    pages = fb1.search_for_pages('Indian Grocery Michigan')
+    fb1.print_page_data(pages, 5)
 
-places4 = requests.get(places3['paging']['next']).json()
-for place in places4['data']:
-    try:
-        if place['emails'] and place['location']['country'] == 'United States':
-            print place['emails'].decode()
-    except:
-        pass
-
-places5 = requests.get(places4['paging']['next']).json()
-for place in places5['data']:
-    try:
-        if place['emails'] and place['location']['country'] == 'United States':
-            print place['emails'].decode()
-    except:
-        pass
